50efb05b80862022fbce0ca1da1436fa9a4e8d99
Daniel Adam <danieladam2002@t-online.de>
2023-04-18 18:54:25 +0200

    Updated EnemyBehavior.cs
    Enemy now patrols set points and chases the player, when in range

--- ExChangeGame/Assets/Scripts/EnemyBehavior.cs
+++ ExChangeGame/Assets/Scripts/EnemyBehavior.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.Serialization;
 using UnityEngine.Timeline;
 
@@ -10,6 +12,7 @@ public enum EnemyState{
     Chase,
     Attack
 }
+
 
 public class EnemyBehavior : MonoBehaviour
 {
@@ -20,14 +23,27 @@ public int MinDist;
 public Transform Player;
 
 public int CheckRadius;
-public LayerMask whatIsGround, whatIsPlayer;
+public LayerMask whatIsPlayer;
+
+public List<Transform> Waypoints;
+private NavMeshAgent _agent;
+[SerializeField] private int currentTarget;
+private bool _reverse;
+[SerializeField] private bool _targetReached;
 
 private bool _playerInSightRange, _playerInAttackRange;
 
-[SerializeField] private EnemyState _currentState = EnemyState.Idle;
+private EnemyState _currentState = EnemyState.Idle;
 
-    private void Update()
+
+private void Start()
+{
+    _agent = GetComponent<NavMeshAgent>();
+}
+
+private void Update()
     {
+        Debug.Log(_currentState);
         switch (_currentState)
         {
             case EnemyState.Idle:
@@ -37,7 +53,7 @@ private bool _playerInSightRange, _playerInAttackRange;
             }
             case EnemyState.Chase:
             {
-                CalculateMovement();
+                ChasePlayer();
                 break;
             }
             case EnemyState.Attack:
@@ -51,30 +67,70 @@ private bool _playerInSightRange, _playerInAttackRange;
 
     private void LookForTargets()
     {
+        Patrol();
         _playerInSightRange = Physics.CheckSphere(transform.position, CheckRadius, whatIsPlayer);
-        _playerInAttackRange = Physics.CheckSphere(transform.position, AttackDist, whatIsPlayer);
-        if (_playerInSightRange && !_playerInAttackRange)
-        {
-            _currentState = EnemyState.Chase;
-        }
+        if (_playerInSightRange) _currentState = EnemyState.Chase;
+        
+    }
 
-        if (_playerInSightRange && _playerInAttackRange)
+    private void Patrol()
+    {
+        _agent.destination = Waypoints[currentTarget].position;
+        float distance = Vector3.Distance(transform.position, Waypoints[currentTarget].position);
+        if (distance < 1f && _targetReached == false)
         {
-            _currentState = EnemyState.Attack;
+            _targetReached = true;
+
+            if (_reverse == false)
+            {
+                currentTarget++;
+            }
+            else
+            {
+                currentTarget--;
+            }
+
+            if (currentTarget == Waypoints.Count - 1)
+            {
+                _reverse = true;
+            }
+            else if (currentTarget == 0)
+            {
+                _reverse = false;
+            }
+        }
+        else if (distance < 1f && _targetReached)
+        {
+            StartCoroutine(WaitBeforeMoving());
         }
     }
 
-    private void CalculateMovement()
+
+    IEnumerator WaitBeforeMoving()
+    {
+        if (currentTarget == Waypoints.Count - 1 || currentTarget == 0)
+        {
+            yield return new WaitForSeconds(2f);
+            _targetReached = false;
+        }
+        else
+        {
+            _targetReached = false;
+        }
+    }
+    
+
+    private void ChasePlayer()
     {
        transform.LookAt(Player);
 
        if (Vector3.Distance(transform.position, Player.position) >= MinDist)
        {
-           transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+           transform.position += transform.forward * (MoveSpeed * Time.deltaTime);
            
            if (Vector3.Distance(transform.position, Player.position) <= AttackDist)
            {
-               _currentState = EnemyState.Attack;
+               //_currentState = EnemyState.Attack; TO-DO
            }
        }
     }
