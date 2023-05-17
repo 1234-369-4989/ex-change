46f05a477cd0f718ec13c00c47e3b7a8d630d910
NishimuraTUT <92289594+NishimuraTUT@users.noreply.github.com>
2023-05-18 03:28:05 +0900

    created prototype minimap

---
+++ ExChangeGame/Assets/Scripts/UI/Bar.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bar : MonoBehaviour
+{
+    //public Character hp_object;
+    [SerializeField]
+    private Image bar, delayBar;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float value, maxValue, delayValue, valueRatio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        value = maxValue;
+        delayValue = maxValue;
+        //for functional test
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        decreaseGuage();
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            DecreaseTest(20);
+        }
+    }
+
+    private void decreaseGuage()
+    {
+        valueRatio = value / maxValue;
+        bar.fillAmount = valueRatio;
+        if(value < delayValue)
+        {
+            delayValue -= Time.unscaledDeltaTime * speed;
+        }
+        delayBar.fillAmount = delayValue / maxValue;
+    }
+
+    public void DecreaseTest(float testDmg)
+    {
+        value -= testDmg;
+        if (value <= 0)
+        {
+            value = 0;
+        }
+    }
+}
