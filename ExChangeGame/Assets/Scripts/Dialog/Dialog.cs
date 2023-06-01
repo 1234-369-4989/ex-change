99e3521b5446a1d6caa35998a8f55857ddc3add0
Markus Fuhlbrügge <74961633+Fylus@users.noreply.github.com>
2023-06-01 14:55:21 +0200

    added dialogsystem

---
+++ ExChangeGame/Assets/Scripts/Dialog/Dialog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Dialog
+{
+    [Serializable]
+    public struct Dialog
+    {
+        public string name;
+        [TextArea(3,10)]
+        public string[] sentences;
+        public UnityEvent onComplete;
+    }
+}
