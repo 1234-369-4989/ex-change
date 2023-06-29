d512f7afb007e3a14cae272619a84958cec1a50b
Daniel Adam <danieladam2002@t-online.de>
2023-06-29 16:05:57 +0200

    Update FOV Editor

--- ExChangeGame/Assets/Scripts/Editors/FOVEditor.cs
+++ ExChangeGame/Assets/Scripts/Editors/FOVEditor.cs
@@ -1,12 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 [CustomEditor(typeof(EnemyBehavior))]
 public class FOVEditor : Editor
 {
-    
-    
-    
+    private void OnSceneGUI()
+    {
+        EnemyBehavior enemyBehavior = (EnemyBehavior)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(enemyBehavior.transform.position, Vector3.up, Vector3.forward, 360, enemyBehavior.CheckRadius);
+    }
 }
