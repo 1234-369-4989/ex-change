2eb122644e5015b417367d1729b9c0d3e1948ccc
NishimuraTUT <92289594+NishimuraTUT@users.noreply.github.com>
2023-06-30 21:40:26 +0900

    Created health and shield bar

--- ExChangeGame/Assets/Scripts/UI/MinimapCamera.cs
+++ ExChangeGame/Assets/Scripts/UI/MinimapCamera.cs
@@ -41,18 +41,21 @@ public class MinimapCamera : MonoBehaviour
         if(Player.transform.position.y < height2to3 && nowLevel != 3)
         {
             mapCamera.cullingMask = 1 << LayerMask.NameToLayer("Level3");
+            mapCamera.cullingMask |= 1 << LayerMask.NameToLayer("Level3Icon");
             mapCamera.cullingMask |= 1 << LayerMask.NameToLayer("Minimap");
             nowLevel = 3;
         }
         else if (height2to3 <Player.transform.position.y && Player.transform.position.y < height1to2 && nowLevel != 2)
         {
             mapCamera.cullingMask = 1 << LayerMask.NameToLayer("Level2");
+            mapCamera.cullingMask |= 1 << LayerMask.NameToLayer("Level2Icon");
             mapCamera.cullingMask |= 1 << LayerMask.NameToLayer("Minimap");
             nowLevel = 2;
         }
         else if (Player.transform.position.y > height1to2  && nowLevel != 1)
         {
             mapCamera.cullingMask = 1 << LayerMask.NameToLayer("Level1");
+            mapCamera.cullingMask |= 1 << LayerMask.NameToLayer("Level1Icon");
             mapCamera.cullingMask |= 1 << LayerMask.NameToLayer("Minimap");
             nowLevel = 1;
         }
