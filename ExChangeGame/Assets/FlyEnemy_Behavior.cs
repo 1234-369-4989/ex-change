d1d64c362502eb4e97e7aef73cc0b09844c873c5
Daniel Adam <danieladam2002@t-online.de>
2023-07-05 09:50:53 +0200

    Create FlyEnemyBehavior

---
+++ ExChangeGame/Assets/FlyEnemy_Behavior.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyEnemy_Behavior : EnemyBehavior
+{
+    public override void Attack()
+    {
+        
+    }
+}
