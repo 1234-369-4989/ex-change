d5199f7934322853fa101a128cfc481801e85662
Markus Fuhlbrügge <74961633+Fylus@users.noreply.github.com>
2023-08-17 12:44:18 +0200

    sound mapsensor & finished minimap

--- ExChangeGame/Assets/Scripts/UI/MinimapCamera.cs
+++ ExChangeGame/Assets/Scripts/UI/MinimapCamera.cs
@@ -23,6 +23,7 @@ public class MinimapCamera : MonoBehaviour
     {
         mapCamera = gameObject.GetComponent<Camera>();
         MainCamera.GetComponent<Camera>().cullingMask &= ~(1 << LayerMask.NameToLayer("Minimap"));
+        mapFloorChange();
         Vector3 pos = Player.transform.position;
         pos.y = 20f;
         gameObject.transform.position = pos;
@@ -45,24 +46,33 @@ public class MinimapCamera : MonoBehaviour
     {
         if(Player.transform.position.y < height2to3 && nowLevel != 3)
         {
-            mapCamera.cullingMask = 1 << LayerMask.NameToLayer("Level3");
+            // mapCamera.cullingMask = 1 << LayerMask.NameToLayer("Level3");
             mapCamera.cullingMask |= 1 << LayerMask.NameToLayer("Level3Icon");
+            // deactivate level 2 and level 1
+            mapCamera.cullingMask &= ~(1 << LayerMask.NameToLayer("Level2Icon"));
+            mapCamera.cullingMask &= ~(1 << LayerMask.NameToLayer("Level1Icon"));
             mapCamera.cullingMask |= 1 << LayerMask.NameToLayer("Minimap");
             nowLevel = 3;
             OnOnLevelChange?.Invoke(3);
         }
         else if (height2to3 <Player.transform.position.y && Player.transform.position.y < height1to2 && nowLevel != 2)
         {
-            mapCamera.cullingMask = 1 << LayerMask.NameToLayer("Level2");
+            // mapCamera.cullingMask = 1 << LayerMask.NameToLayer("Level2");
             mapCamera.cullingMask |= 1 << LayerMask.NameToLayer("Level2Icon");
+            // deactivate level 3 and level 1
+            mapCamera.cullingMask &= ~(1 << LayerMask.NameToLayer("Level3Icon"));
+            mapCamera.cullingMask &= ~(1 << LayerMask.NameToLayer("Level1Icon"));
             mapCamera.cullingMask |= 1 << LayerMask.NameToLayer("Minimap");
             nowLevel = 2;
             OnOnLevelChange?.Invoke(2);
         }
         else if (Player.transform.position.y > height1to2  && nowLevel != 1)
         {
-            mapCamera.cullingMask = 1 << LayerMask.NameToLayer("Level1");
+            // mapCamera.cullingMask = 1 << LayerMask.NameToLayer("Level1");
             mapCamera.cullingMask |= 1 << LayerMask.NameToLayer("Level1Icon");
+            // deactivate level 2 and level 3
+            mapCamera.cullingMask &= ~(1 << LayerMask.NameToLayer("Level2Icon"));
+            mapCamera.cullingMask &= ~(1 << LayerMask.NameToLayer("Level3Icon"));
             mapCamera.cullingMask |= 1 << LayerMask.NameToLayer("Minimap");
             nowLevel = 1;
             OnOnLevelChange?.Invoke(1);
